926d2de0fb6c905ba8b145f35158289c2125bfc4
kavinl <66396017+kavinl@users.noreply.github.com>
2023-05-23 19:40:09 -0400

    Update Metatags.js

--- components/Metatags.js
+++ components/Metatags.js
@@ -4,7 +4,7 @@ export default function MetaTags() {
       <meta name="viewport" content="width=device-width,initial-scale=1" />
 
       {/* <!-- Primary Meta Tags --> */}
-      <meta name="title" content="Women who Build" />
+      <meta name="title" content="Women Who Build" />
       <meta
         name="description"
         content="A repository to celebrate the women and nonbinary people building and investing for the future."
@@ -13,7 +13,7 @@ export default function MetaTags() {
       {/* <!-- Open Graph / Facebook --/> */}
       <meta property="og:type" content="website" />
       <meta property="og:url" content="http://brazilianswho.design" />
-      <meta property="og:title" content="Brazilians Who Design" />
+      <meta property="og:title" content="Women Who Build" />
       <meta
         property="og:description"
         content="A repository to celebrate the women and nonbinary people building and investing for the future."
@@ -26,7 +26,7 @@ export default function MetaTags() {
       {/* <!-- Twitter --/> */}
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content="http://brazilianswho.design" />
-      <meta property="twitter:title" content="Brazilians Who Design" />
+      <meta property="twitter:title" content="Women Who Build" />
       <meta
         property="twitter:description"
         content="A repository to celebrate the women and nonbinary people building and investing for the future."
